56b8d2f0a0c6ca0454ee12e0824ee18336e52880
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-07-23 21:08:36 +0600

    update

--- frontend/src/pages/Auth/AuthForm/AuthForm.jsx
+++ frontend/src/pages/Auth/AuthForm/AuthForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import useAuth from "../../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import { FaGoogle, FaTwitter } from "react-icons/fa";
 
 const AuthForm = ({ mode }) => {
   const {
@@ -118,7 +119,7 @@ const AuthForm = ({ mode }) => {
   };
 
   return (
-    <div className=" flex items-center justify-center bg-gradient-to-tr from-blue-50 to-purple-100 px-4">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-tr from-blue-50 to-purple-100 px-4">
       <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           {mode === "signin" ? "Sign In to Your Account" : "Create a New Account"}
@@ -187,12 +188,16 @@ const AuthForm = ({ mode }) => {
           <hr className="flex-grow border-t border-gray-300" />
         </div>
 
-        <div className="flex gap-3">
-          <button onClick={handleGoogleSignIn} className="flex-1 border rounded-xl py-2 hover:bg-gray-50 transition text-sm">
-            Continue with Google
+        <div className="flex justify-between gap-3">
+          <button onClick={handleGoogleSignIn} className="flex justify-center items-center gap-1 w-1/2  border rounded-xl py-2 hover:bg-gray-50 transition text-sm">
+            <span ><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
+<path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+</svg></span> Google
           </button>
-          <button onClick={handleTwitterSignIn} className="flex-1 border rounded-xl py-2 hover:bg-gray-50 transition text-sm">
-            Continue with Twitter
+          <button onClick={handleTwitterSignIn} className="flex justify-center items-center gap-1 w-1/2 border rounded-xl py-2 hover:bg-gray-50 transition text-sm">
+         <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 30 30">
+<path d="M 6 4 C 4.895 4 4 4.895 4 6 L 4 24 C 4 25.105 4.895 26 6 26 L 24 26 C 25.105 26 26 25.105 26 24 L 26 6 C 26 4.895 25.105 4 24 4 L 6 4 z M 8.6484375 9 L 13.259766 9 L 15.951172 12.847656 L 19.28125 9 L 20.732422 9 L 16.603516 13.78125 L 21.654297 21 L 17.042969 21 L 14.056641 16.730469 L 10.369141 21 L 8.8945312 21 L 13.400391 15.794922 L 8.6484375 9 z M 10.878906 10.183594 L 17.632812 19.810547 L 19.421875 19.810547 L 12.666016 10.183594 L 10.878906 10.183594 z"></path>
+</svg> </span>  Twitter
           </button>
         </div>
 
